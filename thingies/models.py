59729205e3a3a3ae27b0e712f17de0249386c58c
Daniel Fairhead <daniel@dev.ngo>
2025-01-27 21:05:29 +0000

    Fix ranking, etc.

--- thingies/models.py
+++ thingies/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import F
 from django.contrib.postgres.search import (
     SearchVector,
     SearchQuery,
@@ -11,8 +12,19 @@ class ThingQuerySet(models.QuerySet):
     def search(self, query):
         query = SearchQuery(query)
         return (
-            self.annotate(rank=SearchRank("search_vector", query))
+            self.annotate(
+                rank=SearchRank(
+                    F("search_vector"),
+                    query,
+                    # Set the weights for each letter D,C,B,A
+                    weights=[0.1, 0.2, 0.3, 1.0],
+                    # Don't normalize by document-length:
+                    normalization=0,
+                )
+            )
+            # Only include results with any kind of match:
             .filter(rank__gt=0.00001)
+            # Order by closest match:
             .order_by("-rank")
         )
 
@@ -28,5 +40,7 @@ class Thing(models.Model):
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
         Thing.objects.filter(pk=self.pk).update(
-            search_vector=SearchVector("title", "subtitle", "description")
+            search_vector=SearchVector("title", weight="A")
+            + SearchVector("subtitle", weight="B")
+            + SearchVector("description", weight="C")
         )
